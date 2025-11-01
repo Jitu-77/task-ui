4a409b020ad273741200c6e488e65a2d0a037f60
Jitu-77 <jyotirmoypal077@gmail.com>
2025-11-01 14:10:10 +0530

    sign up form is done

--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -1,10 +1,57 @@
-import React from 'react'
+import React ,{useState} from 'react'
 import './SignUp.css'
 function SignUp() {
+  const [fileName, setFileName] = useState('');
+  const handleSubmit=()=>{
+
+  }
+  const initialValues = {
+    username :"",
+    password:"",
+    email:"",
+    avatar:"",
+  }
+  
+  const handleFileChange = (e) => {
+    console.log(e,"EEE")
+    // setFileName(e.target.files[0]?.name || '');
+  };
+  const handleClick = (e) => {
+    console.log(e,"CCC")
+    // setFileName(e.target.files[0]?.name || '');
+  };
   return (
     <div className='signUpContainer'>
         <div className='cardContainer'> </div>
-        <div className='buttonContainer'> </div>
+        <div className='formContainer'> 
+            <div className='formContainerSection'>
+              <div className='inputField'>
+              <label>Email</label>
+              <input className='inputStyle' type='text'/>
+              </div>
+
+              <div className='inputField'>
+              <label>Password</label>
+              <input className='inputStyle' type='text'/>
+              </div>
+
+              <div className='inputField'>
+              <label>Email</label>
+              <input className='inputStyle' type='text'/>
+              </div>
+
+              <div className='inputField'>
+              <label>Avatar</label>
+              <label htmlFor="avatar" className='customFileUpload'>Choose Avatar</label>              
+              <input className='inputFileStyle' type='file' onChange={handleFileChange} onClick={handleClick}/>
+              {fileName && <p className="fileName">{fileName}</p>}
+              </div>
+
+              <div className='submitForm'>
+                Submit
+              </div>
+            </div>        
+        </div>
     </div>
   )
 }
