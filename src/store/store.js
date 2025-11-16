856b3e5e9a9527ad744c162af499fcfa1dd68ca9
Jitu-77 <jyotirmoypal077@gmail.com>
2025-11-16 11:21:10 +0530

    react store set up is in WIP

--- src/store/store.js
+++ src/store/store.js
@@ -1,9 +1,16 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore, isAction } from "@reduxjs/toolkit";
+
+// main components 
+// Action
+// Reducer
+// Store
+// Dispatcher , Selector
+
 
 
 // 1>create a store 
 // 2> create a reducer -- slices (in diff files)
-//3> make a n initial state
+//3> make a initial state
 //4> create a slice -- define property , -- name , initialState, reducers -- contains properties and functions
 
 
